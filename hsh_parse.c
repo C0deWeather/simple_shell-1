a3d4697225ab8a57536fcff9cd7b1fde49bec894
Frankacer <frankbright211@gmail.com>
2023-09-09 17:15:27 -0700

    Just added the parse_line function

---
+++ hsh_parse.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * parse_line - split input string into tokens
+ * @line: input string
+ *
+ * Return: returns a pointer to the array of tokens, otherwise NULL.
+*/
+char **parse_line(char *line)
+{
+	int i = 0;
+	char delimiter[] = " ";
+	char **tokens = malloc(2 * sizeof(char *));
+
+	/*check if dynamic memory allocation was successful*/
+	if (tokens == NULL)
+	{
+		fprintf(stderr, "Dynamic memory allocation failed!\n");
+		return (NULL);
+	}
+	if (line == NULL)
+	{
+		fprintf(stderr, "Input line cannot be NULL!\n");
+		return (NULL);
+	}
+	/*tokenize line*/
+	tokens[i] = strtok(line, delimiter);
+	while (token[i] != NULL)
+	{
+		i++;
+		tokens = realloc(tokens, (i + 1) * sizeof(char *));
+		if (tokens == NULL)
+		{
+			fprintf(stderr, "Dynamic memory allocation failed!\n");
+			return (NULL);
+		}
+		tokens[i] = strtok(NULL, delimiter);
+	}
+	return (tokens);
+}
+
