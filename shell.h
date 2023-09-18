3a2ce0d3cfabd8896b56db260a12aa59d37084cd
Frankacer <frankbright211@gmail.com>
2023-09-18 08:26:20 -0700

    Updated the header file

--- shell.h
+++ shell.h
@@ -21,9 +21,17 @@ typedef struct token
 	char **args;
 } token;
 
+extern char *PATH;
 token parse_line(char *line);
 char **parse(char *str);
 void hsh_loop(void);
 int print_prompt(char *prompt);
+char *find_exe(char *cmd);
+char *_strcat(char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
 
 #endif
+
