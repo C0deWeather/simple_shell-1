063780893f5e7d8a7c8f773002b9a1d3fd0f5a2f
emma-obelenge <obelemmanuel@gmail.com>
2023-09-18 12:09:30 -0700

    updated the cmd_exec.c file and shell.c file

--- cmd_exec.c
+++ cmd_exec.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
  * cmd_exec - resposible for cmd execution in forked process
- * usr_input: token type of data taken as parameter
+ * @usr_input: token type of data taken as parameter
+ * @abs_path: the environment variable passed through main
+ * Return: will return 0 upon success and 1 upon failure
  * Do not forget to free temp_cmd buffer
  */
-int cmd_exec(char usr_input)
+int cmd_exec(char abs_path, token usr_input)
 {
 	pid_t pid;
 	int status;
 	char **temp_cmd = malloc(sizeof(usr_input));
 
 	*temp_cmd[] = {usr_input.cmd, NULL};
-	for (i = 1, usr_input.args, i++)
-		temp_cmd[i] = usr_input.args[i - 1];
-	
+	for (i = 0, usr_input.args[i], i++)
+		temp_cmd[i + 1] = usr_input.args[i];
+
 	pid = fork();
 	if (pid == -1)
 	{
@@ -22,18 +24,15 @@ int cmd_exec(char usr_input)
 	}
 	if (pid == 0)
 	{
-		if (execve(usr_input.cmd, temp_cmd, env) = -1)
+		if (execve(abs_path, temp_cmd, NULL) == -1)
 		{
 			perror("argv[0]: ");
 			return (1);
 		}
 	}
-	if (!(pid <=0))
+	if (!(pid <= 0))
 	{
 		wait(&status);
 	}
-	/**
-	 * Remember to free temp_cmd
-	 */
 	return (0);
 }
